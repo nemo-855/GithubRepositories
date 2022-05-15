57bace53f8178d992d2429460886913e160f1941
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-15 14:51:23 +0900

    feat: uiモジュール内部の依存関係をhiltで解決

--- ui/src/main/java/com/nemo/githubrepositories/main/MainListViewModel.kt
+++ ui/src/main/java/com/nemo/githubrepositories/main/MainListViewModel.kt
@@ -2,7 +2,10 @@ package com.nemo.githubrepositories.main
 
 import androidx.lifecycle.ViewModel
 import com.nemo.data.repositories.interfaces.GithubRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainListViewModel(
+@HiltViewModel
+class MainListViewModel @Inject constructor(
     private val githubRepository: GithubRepository
 ) : ViewModel()
