41c4b6e0afc9b13b466996849dc01cd203f2c78f
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-15 17:23:30 +0900

    feat: MainListProjectItemを実装

--- ui/src/main/java/com/nemo/githubrepositories/main/list/MainListViewModel.kt
+++ ui/src/main/java/com/nemo/githubrepositories/main/list/MainListViewModel.kt
@@ -3,9 +3,22 @@ package com.nemo.githubrepositories.main.list
 import androidx.lifecycle.ViewModel
 import com.nemo.data.repositories.interfaces.GithubRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.ZonedDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class MainListViewModel @Inject constructor(
     private val githubRepository: GithubRepository
-) : ViewModel()
+) : ViewModel() {
+    sealed class MainListUiModel {
+        object ProgressIndicatorUiModel : MainListUiModel()
+
+        data class ProjectUiModel(
+            val id: Int,
+            val name: String,
+            val isPrivate: Boolean,
+            val ownerName: String,
+            val createdUnixTime: ZonedDateTime
+        ) : MainListUiModel()
+    }
+}
