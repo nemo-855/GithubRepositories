579b646c60ce059c0cd0b1f539aa2869e1e9a106
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2022-05-15 18:46:41 +0900

    feat: MainListFragmentの一番最初の画面を作成した

--- ui/src/main/java/com/nemo/githubrepositories/main/list/MainListViewModel.kt
+++ ui/src/main/java/com/nemo/githubrepositories/main/list/MainListViewModel.kt
@@ -9,6 +9,9 @@ import androidx.lifecycle.viewModelScope
 import com.nemo.data.models.GithubProject
 import com.nemo.data.repositories.interfaces.GithubRepository
 import com.nemo.githubrepositories.R
+import com.nemo.githubrepositories.main.list.MainListViewModel.MainListUiModel.ProgressIndicatorUiModel
+import com.nemo.githubrepositories.main.list.MainListViewModel.MainListUiModel.ProjectUiModel
+import com.nemo.githubrepositories.main.list.MainListViewModel.MainListUiModel.TextAndImageUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.time.ZonedDateTime
@@ -18,18 +21,26 @@ import javax.inject.Inject
 class MainListViewModel @Inject constructor(
     private val githubRepository: GithubRepository
 ) : ViewModel() {
-    private val _uiModelListLD = MutableLiveData<List<MainListUiModel>>(listOf(MainListUiModel.ProgressIndicatorUiModel))
+    private val _uiModelListLD = MutableLiveData<List<MainListUiModel>>(
+        listOf(
+            TextAndImageUiModel(
+                textResId = R.string.input_username,
+                imageResId = R.drawable.magnifying_glass
+            )
+        )
+    )
     val uiModelListLD: LiveData<List<MainListUiModel>>
         get() = _uiModelListLD
 
     fun onClickSearchButton(userName: String) {
         viewModelScope.launch {
+            _uiModelListLD.value = listOf(ProgressIndicatorUiModel)
             runCatching {
                 // FIXME: 入力できるようにする
                 val fetchedProjectList = githubRepository.fetchGithubProjects(userName)
                 _uiModelListLD.value = when (fetchedProjectList.isEmpty()) {
                     true -> listOf(
-                        MainListUiModel.TextAndImageUiModel(
+                        TextAndImageUiModel(
                             textResId = R.string.search_result_is_empty,
                             imageResId = R.drawable.crying_face
                         )
@@ -40,7 +51,7 @@ class MainListViewModel @Inject constructor(
                 }
             }.onFailure {
                 _uiModelListLD.value = listOf(
-                    MainListUiModel.TextAndImageUiModel(
+                    TextAndImageUiModel(
                         textResId = R.string.failed_connection,
                         imageResId = R.drawable.loudly_crying_face
                     )
@@ -49,7 +60,7 @@ class MainListViewModel @Inject constructor(
         }
     }
 
-    private fun GithubProject.toProjectUiModel() = MainListUiModel.ProjectUiModel(
+    private fun GithubProject.toProjectUiModel() = ProjectUiModel(
         id = id,
         name = name,
         isPrivate = isPrivate,
