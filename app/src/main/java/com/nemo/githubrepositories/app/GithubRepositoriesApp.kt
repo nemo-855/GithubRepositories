d776c5d899b39bc0817233aa55993cd9552bf46f
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-09 19:54:09 +0900

    feat: kmmモジュールを定義した

--- app/src/main/java/com/nemo/githubrepositories/app/GithubRepositoriesApp.kt
+++ app/src/main/java/com/nemo/githubrepositories/app/GithubRepositoriesApp.kt
@@ -1,14 +1,13 @@
 package com.nemo.githubrepositories.app
 
 import android.app.Application
+import com.nemo.githubrepositories_kmm.di.initDi
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GithubRepositoriesApp : Application() {
     override fun onCreate() {
         super.onCreate()
-
-        //FIXME: これをどうにかして呼び出す
-        //initDi()
+        initDi()
     }
 }
