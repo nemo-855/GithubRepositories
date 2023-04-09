8d99089646a3f1257ed2ccfa4f4e4d63caa880bd
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-09 17:54:06 +0900

    feat: appモジュールをアプリケーションモジュールとして追加

--- app/src/main/java/com/nemo/githubrepositories/app/GithubRepositoriesApp.kt
+++ app/src/main/java/com/nemo/githubrepositories/app/GithubRepositoriesApp.kt
@@ -1,13 +1,14 @@
-package com.nemo.githubrepositories
+package com.nemo.githubrepositories.app
 
 import android.app.Application
-import com.nemo.githubrepositories_kmm.di.initDi
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GithubRepositoriesApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        initDi()
+
+        //FIXME: これをどうにかして呼び出す
+        //initDi()
     }
 }
