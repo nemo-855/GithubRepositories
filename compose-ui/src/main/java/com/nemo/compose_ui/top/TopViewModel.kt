87f6c8c1744dd82bd92c04de2fecd858cda21d3d
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-11 18:20:23 +0900

    feat: TopViewModelを定義

---
+++ compose-ui/src/main/java/com/nemo/compose_ui/top/TopViewModel.kt
@@ -0,0 +1,14 @@
+package com.nemo.compose_ui.top
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class TopViewModel @Inject constructor(): ViewModel() {
+    private val _uiState = MutableStateFlow(TopUiState.initial())
+    val uiState: StateFlow<TopUiState> = _uiState.asStateFlow()
+}
