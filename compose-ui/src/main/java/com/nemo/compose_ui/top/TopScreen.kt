8ceb62e290703a6ddbf78d31bbdee6fb44d421b6
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-11 18:24:40 +0900

    feat: ユーザー名を入力を促すコンポーネントの作成

--- compose-ui/src/main/java/com/nemo/compose_ui/top/TopScreen.kt
+++ compose-ui/src/main/java/com/nemo/compose_ui/top/TopScreen.kt
@@ -1,9 +1,72 @@
 package com.nemo.compose_ui.top
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.nemo.compose_ui.designsystem.GithubTypography
+import com.nemo.githubrepositories.composeui.R
 
 @Composable
-fun TopScreen(modifier: Modifier = Modifier) {
+fun TopScreen(
+    modifier: Modifier = Modifier,
+    viewModel: TopViewModel = hiltViewModel(),
+) {
+    val state: TopUiState by viewModel.uiState.collectAsState()
 
+    TopScreenContent(
+        modifier = modifier,
+        uiState = state,
+    )
+}
+
+@Composable
+fun TopScreenContent(
+    uiState: TopUiState,
+    modifier: Modifier,
+) {
+    if (uiState.isInitial) {
+        Column(modifier = modifier) {
+            Spacer(modifier = Modifier.height(32.dp))
+            SearchUsername()
+        }
+    }
+}
+
+@Composable
+private fun SearchUsername() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_search_24),
+            contentDescription = stringResource(id = R.string.search_image),
+            modifier = Modifier
+                .width(300.dp)
+                .aspectRatio(1f),
+            contentScale = ContentScale.Crop,
+        )
+        Text(
+            text = stringResource(id = R.string.input_username),
+            style = GithubTypography.bodyLarge,
+        )
+    }
 }
