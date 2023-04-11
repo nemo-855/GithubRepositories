491a7e5557e57718ee16d7d8ed281f14a7f139b9
nemo-855 <77588574+nemo-855@users.noreply.github.com>
2023-04-11 18:19:28 +0900

    feat: ColorPalletteの命名を変更

--- compose-ui/src/main/java/com/nemo/compose_ui/designsystem/GithubColorPalette.kt
+++ compose-ui/src/main/java/com/nemo/compose_ui/designsystem/GithubColorPalette.kt
@@ -2,7 +2,7 @@ package com.nemo.compose_ui.designsystem
 
 import androidx.compose.ui.graphics.Color
 
-object ColorPalette {
+object GithubColorPalette {
     val BlueGrey900 = Color(0xFF263238)
     val BlueGrey800 = Color(0xFF37474F)
     val BlueGrey700 = Color(0xFF455A64)
